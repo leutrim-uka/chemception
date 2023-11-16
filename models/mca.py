d2e8668d4047d6e12108bd39459eb76ec225c980
Leutrim Uka <uka@uni-potsdam.de>
2023-11-16 17:36:54 +0100

    Implement drug_fetcher

---
+++ models/mca.py
@@ -0,0 +1,7 @@
+import torch
+
+# dense attention layer
+# embedding layer
+# sequence attention layer
+# contextual attention layer
+# contextual attention matrix layer
--- src/drug_fetcher.py
+++ src/drug_fetcher.py
@@ -2,30 +2,33 @@ import pandas as pd
 import numpy as np
 import requests
 import time
+from tqdm import tqdm
 
 from rdkit.Chem import AllChem
 from rdkit.Chem import MolFromSmiles
 
-file = pd.read_csv('data/dosage.csv', delimiter=";", usecols=['DRUG_NAME'])
 
-
-def get_smiles(drug_name: str) -> str:
+def get_smiles(drug_name: str) -> str | None:
     """
     Get SMILES encoding from PubChem for a given drug name
     (!!! MAX. 5 REQUESTS PER SECOND)
     :param str drug_name: A string with the drug name
-    :return str smiles: A string with SMILES encoding
+    :return str|None smiles: A string with SMILES encoding
     """
 
     # SMILES encodings can be found in this URL in PubChem's website
     url = f'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/{drug_name}/property/CanonicalSMILES/TXT'
     # Send a GET request to the server and get the SMILES
-    smiles = requests.get(url).text
-    # Remove newlines and spaces from SMILES string
-    smiles = smiles.replace('\n', '')
-    smiles = smiles.replace(' ', '')
+    pubchem_response = requests.get(url).text
 
-    return smiles
+    if 'Status: 404' in pubchem_response:
+        return None
+
+    # Remove newlines and spaces from SMILES string
+    pubchem_response = pubchem_response.replace('\n', '')
+    pubchem_response = pubchem_response.replace(' ', '')
+
+    return pubchem_response
 
 
 def get_2d_images(drug_name: str, width: int = 80, height: int = 80) -> bytes:
@@ -63,4 +66,34 @@ def smiles2image(smiles: str, width: int, height: int):
     pass
 
 
+if __name__ == '__main__':
 
+    # Name of column where drug names can be found
+    drug_col = 'DRUG_NAME'
+
+    # Path to data source
+    file = pd.read_csv('../data/unprocessed/dosage.csv',
+                       delimiter=";",
+                       usecols=[drug_col]
+                       )
+
+    smiles_mapping = {}  # Keys = drug names, values = corresponding SMILES embedding
+    unique_drugs = file[drug_col].unique()  # List of unique drugs in the given dataset
+    no_unique_drugs = len(unique_drugs)  # Number of unique drugs
+
+    for i, drug in enumerate(tqdm(unique_drugs)):
+        response = get_smiles(drug)
+        smiles_mapping[drug] = response
+
+        # Sleep 1 second every 5 requests (per PubChem's request)
+        if i % 5 == 0:
+            time.sleep(1)
+
+    not_found_count = list(smiles_mapping.values()).count(None)
+
+    print(f"{not_found_count} out of {no_unique_drugs} drugs were not found on PubChem!\n"
+          f"Please check the spelling of the drug name in the dataset.")
+
+    smiles_df = pd.DataFrame(list(smiles_mapping.items()), columns=['drug', 'smiles'])
+
+    smiles_df.to_csv('../data/smiles.csv', index=False)
