17ab2a6b95ac6abdae57be73c5163f588f20cada
Leutrim Uka <leutrim.uka6@gmail.com>
2023-11-25 00:28:07 +0100

    Implement layers

---
+++ src/layers/ContextualAttentionLayer.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+
+class ContextualAttentionLayer(nn.Module):
+    def __init__(self):
+        super(ContextualAttentionLayer, self).__init__()
+
+    def forward(self):
+        pass
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,2 @@
 def embed_tokens(*kwargs):
     pass
-
-
-def contextual_attention_layer(*kwargs):
-    pass
-
-
-def dense_attention_layer(*kwargs):
-    pass
